9fa64f101b097b18491af5cdd69c8c960e94f8d6
accenturedanialmirza <danial.m.bin.madrawi@accenture.com>
2025-06-06 15:36:34 +0800

    servie.py working now

--- fastapi_app/service.py
+++ fastapi_app/service.py
@@ -1,7 +1,7 @@
-from fastapi_app.fallback_tool import create_tool_node_with_fallback
-from fastapi_app.models import State, OPENAI_MODEL, Request, Response
-from fastapi_app.dataframe import create_dataframe_pd_json
-from fastapi_app.sql_operations import list_tables, describe_table, run_sql_query
+from fallback_tool import create_tool_node_with_fallback
+from models import State, OPENAI_MODEL, Request, Response
+from dataframe import create_dataframe_pd_json
+from sql_operations import list_tables, describe_table, run_sql_query
 
 from sqlalchemy import create_engine
 
@@ -382,7 +382,7 @@ async def main(request: Request) -> Response:
     # )
 
     result = app.invoke(
-        {"messages": [("user", request.query)]}
+        {"messages": [("user", request["query"])]}
     )
 
     # print(result)
@@ -430,8 +430,8 @@ async def main(request: Request) -> Response:
 #     print("--Code Generated--")
 #     print(messages["code_generated"])
 
-# import asyncio
+import asyncio
 
-# # if __name__=="__main__":
-# request = {"query":"Total number of albums for each artist with pop genre"}
-# response = asyncio.run(main(request))
+if __name__=="__main__":
+    request = {"query":"Find the total sales for each artist using bar chart"}
+    response = asyncio.run(main(request))
